0474107f9cf6d532ba96dd16c7fcf950da15075c
addee1fe <addee1fe@users.noreply.github.com>
2021-01-18 23:31:25 +0200

    refactor p0001 solution

--- solutions/p0001/solution.go
+++ solutions/p0001/solution.go
@@ -1,10 +1,10 @@
 package solution
 
 func twoSum(nums []int, target int) []int {
-	index := make(map[int]int)
+	index := make(map[int]int, 0)
 	for i, v := range nums {
-		if _, ok := index[target-v]; ok {
-			return []int{index[target-v], i}
+		if val, ok := index[target-v]; ok {
+			return []int{val, i}
 		}
 		index[v] = i
 	}
