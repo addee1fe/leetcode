d2cf830a7ee6c9c0aebdea61d3885c1bac942d3e
addee1fe <addee1fe@users.noreply.github.com>
2021-03-10 12:10:27 +0200

    added binary search version to p0069

--- solutions/p0069/solution.go
+++ solutions/p0069/solution.go
@@ -13,3 +13,17 @@ func mySqrt(x int) int {
 	}
 	return int(z)
 }
+
+// binary search
+// func mySqrt(x int) int {
+//     l, r := 0, x
+//     for l < r{
+//         m := (l + r + 1) / 2
+//         if m > x /m {
+//             r = m - 1
+//         } else {
+//             l = m
+//         }
+//     }
+//     return r
+// }
